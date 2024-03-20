12ca5927e5660abdb8d1e8e0cdcd66b008138a44
TheFatAndFurious <mrguerrilla@gmail.com>
2024-03-20 23:57:26 +0100

    models

--- src/models/users.ts
+++ src/models/users.ts
@@ -2,10 +2,14 @@ import type { User } from "../../types";
 import { db } from "../data/db";
 
 async function createUser(user: User): Promise<void> {
-  const { username, password, email, first_name, last_name } = user;
-  const query = db.prepare(
-    "INSERT INTO users (username, password, email, first_name, last_name) VALUES (?,?,?,?,?)"
-  );
-  query.run(username, password, email, first_name, last_name);
-  query.finalize();
+  try {
+    const { username, password, email, first_name, last_name } = user;
+    const query = db.prepare(
+      "INSERT INTO users (username, password, email, first_name, last_name) VALUES (?,?,?,?,?)"
+    );
+    query.run(username, password, email, first_name, last_name);
+    query.finalize();
+  } catch (error) {
+    console.error(error);
+  }
 }
